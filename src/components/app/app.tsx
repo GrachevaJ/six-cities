4fb584a4801a8dd40d9c79fc127356da16da5708
GrachevaJ <gra.che.va@yandex.ru>
2026-02-23 15:19:57 +0700

    Добавляет компонент приватного маршрута

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,10 +1,10 @@
 import { Route, BrowserRouter, Routes} from 'react-router-dom';
-import { AppRoute } from '../../const';
+import { AppRoute, AuthorizationStatus } from '../../const';
 import MainPage from '../../pages/main/main';
 import Login from '../../pages/login-page/login';
 import Favorites from '../../pages/favorites/favorites';
 import NotFound from '../not-found/not-found';
-
+import PrivateRoute from '../private-route/private-route';
 
 type AppProps = {
   offersCount: number;
@@ -14,10 +14,28 @@ function App({ offersCount}: AppProps): JSX.Element {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={AppRoute.Main} element={<MainPage offersCount={offersCount} />} />
-        <Route path={AppRoute.Login} element={<Login />} />
-        <Route path={AppRoute.Favorites} element={<Favorites />}/>
-        <Route path="*" element={<NotFound />}/>
+        <Route
+          path={AppRoute.Main}
+          element={<MainPage offersCount={offersCount} />}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<Login />}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={
+            <PrivateRoute
+              authorizationStatus={AuthorizationStatus.NoAuth}
+            >
+              <Favorites />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="*"
+          element={<NotFound />}
+        />
       </Routes>
     </BrowserRouter>
   );
