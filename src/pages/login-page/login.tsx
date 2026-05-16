59131633aaebed9f9e1cbbbcf28cc3926681a20e
GrachevaJ <gra.che.va@yandex.ru>
2026-05-16 17:20:43 +0700

    Тестирует маршрутизацию

--- src/pages/login-page/login.tsx
+++ src/pages/login-page/login.tsx
@@ -28,12 +28,12 @@ function Login(): JSX.Element {
             <h1 className="login__title">Sign in</h1>
             <form className="login__form form" action="#" method="post" onSubmit={handleFormSubmit}>
               <div className="login__input-wrapper form__input-wrapper">
-                <label className="visually-hidden">E-mail</label>
-                <input className="login__input form__input" type="email" name="email" placeholder="Email" required />
+                <label className="visually-hidden" htmlFor="email">E-mail</label>
+                <input className="login__input form__input" type="email" name="email" id="email" placeholder="Email" required />
               </div>
               <div className="login__input-wrapper form__input-wrapper">
-                <label className="visually-hidden">Password</label>
-                <input className="login__input form__input" type="password" name="password" placeholder="Password" required />
+                <label className="visually-hidden" htmlFor="password">Password</label>
+                <input className="login__input form__input" type="password" name="password" id="password" placeholder="Password" required />
               </div>
               <button className="login__submit form__submit button" type="submit">Sign in</button>
             </form>
